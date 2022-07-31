11ba8346f57abcd23cc31ab2128e74baf87e394e
Nagarjuna927 <105323732+Nagarjuna927@users.noreply.github.com>
2022-07-31 01:02:01 -0500

    worked on thankyou on successful submit of take survey

--- src/router.js
+++ src/router.js
@@ -6,6 +6,12 @@ const routes = [
     component: () => import('./views/TakeSurvey/TakeSurvey.vue'),
     props: true
   },
+  {
+    name: 'responseRecorded',
+    path: '/response-recorded',
+    component: () => import('./views/ResponseRecorded/ResponseRecorded.vue'),
+    props: true
+  },
 ];
 const router = createRouter({
   base: process.env.NODE_ENV === 'development' ? '/' : '/survey-frontend-survey/',
---
+++ src/views/ResponseRecorded/ResponseRecorded.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="create__survey">
+    <div class="survey__question">
+      <h1 class="thankyou">Your response has been recorded</h1>
+    </div>
+  </div>
+</template>
+<style>
+.thankyou {
+  color: var(--white);
+  text-align: center;
+}
+</style>
